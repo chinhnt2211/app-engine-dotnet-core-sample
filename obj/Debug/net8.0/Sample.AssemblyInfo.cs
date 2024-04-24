c7029fd7ad7aa47c4d5554e964d3be91a75b5f11
chinhnt32@fpt.com <chinhnt32@fpt.com>
2024-04-24 15:20:09 +0700

    update

--- obj/Debug/net8.0/Sample.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c585e5b41fc4ea6507514a83dacf1f25d3e74a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00df9fdfd18da4f743a4e179516fad89650d5b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
